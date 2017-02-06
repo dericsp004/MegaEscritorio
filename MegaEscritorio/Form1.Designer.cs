921df4123dfa31b76d67aae921c66a167947836e
deric <dericsp004@gmail.com>
2017-02-05 22:38:56 -0700

    Minor header change

--- MegaEscritorio/Form1.Designer.cs
+++ MegaEscritorio/Form1.Designer.cs
@@ -205,7 +205,7 @@
             this.Controls.Add(this.comSelectDrawers);
             this.Controls.Add(this.comSelectDays);
             this.Name = "Form1";
-            this.Text = "s";
+            this.Text = "MegaEscritorio - Deric Plummer, Scott Bailey";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
